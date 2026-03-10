dabce38e9d4b9edaf7f4f4ca3a2a07a88257ff2e
viratlogistics <viratlogistics0820@gmail.com>
2026-03-10 23:27:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -293,7 +293,7 @@ elif menu == "2. LR Entry":
                 hc = st.number_input("Hired Charges")
                 dsl = toll = drv = 0.0
 
-if st.form_submit_button("🚀 SAVE LR"):
+    if st.form_submit_button("🚀 SAVE LR"):
             if bill_pty and bill_pty != "Select" and fr_amt > 0:
                 # 1. Branch ki details fetch karo
                 br_info = df_m[df_m['Name'] == sel_br].iloc[0] if sel_br != "Select" else {}
@@ -541,6 +541,7 @@ elif menu == "7. Driver Khata":
                 total_p = pd.to_numeric(d_hist['Amount'], errors='coerce').sum() if not d_hist.empty else 0
                 st.warning(f"Total Personal Dues: ₹{total_p:,.2f}")
                 st.dataframe(d_hist, use_container_width=True, hide_index=True)
+
 
 
 
