015a3c10971e0b01743ab3bd3ba783504a932b67
viratlogistics <viratlogistics0820@gmail.com>
2026-03-11 10:07:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -568,6 +568,7 @@ elif menu == "7. Driver Khata":
                 total_p = pd.to_numeric(d_hist['Amount'], errors='coerce').sum() if not d_hist.empty else 0
                 st.warning(f"Total Personal Dues: ₹{total_p:,.2f}")
                 st.dataframe(d_hist, use_container_width=True, hide_index=True)
+                
 elif menu == "8. Monthly Billing":
     st.header("🧾 Monthly Billing & Invoice Generation")
     
@@ -643,6 +644,7 @@ elif menu == "8. Monthly Billing":
             file_name=f"Invoice_{st.session_state.inv_ready['InvNo']}.pdf",
             mime="application/pdf"
         )
+
 
 
 
